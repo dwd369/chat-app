b1b493accb65aec0c4dc46848c3a144c1b24a344
dwd369 <dwd7290@gmail.com>
2023-12-20 23:38:38 -0800

    update Chat.js to include offline mode for loading messages from AsyncStorage and disable keyboard

--- components/Chat.js
+++ components/Chat.js
@@ -5,18 +5,80 @@ import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Platform, KeyboardAvoidingView, Keyboard, TouchableOpacity, SectionList, Alert } from "react-native";
 
 // import 3rd party component
-import { GiftedChat, Bubble } from "react-native-gifted-chat"; // gifted-chat
+import { GiftedChat, Bubble, renderInputToolbar, InputToolbar } from "react-native-gifted-chat"; // gifted-chat
 
 // import Firebase
 import { collection, getDocs, addDoc, onSnapshot, query, where, orderBy } from "firebase/firestore";
+import { AsyncStorage } from "@react-native-async-storage/async-storage"
 
-const Chat = ({ db, route, navigation }) => {
+const Chat = ({ db, route, navigation, isConnected }) => {
 
     // extract props
     const { userID, name, bgColor } = route.params;  // backgroundColor for view
 
     // define useState
     const [messages, setMessages] = useState([]);
+
+    // initialize global variable to hold onSnap function in useEffect
+    let unsubMessages;
+
+    // set chatroom title to user specified name
+    useEffect(() => {
+        navigation.setOptions({ title: name });
+    }, []);
+
+    // load static system message and user message
+    useEffect(() => {
+
+        if (isConnected === true) {
+
+            // unregister current onSnapshot() listener to avoid registering multiple listeners when
+            // useEffect code is re-executed
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+
+            // define query to fetch messages object and sort by createdAt descending
+            const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+
+            // detect messages object changes and update messages object
+            unsubMessages = onSnapshot(q, (documentSnapshot) => {
+                // load messages
+                let newMessages = [];
+                documentSnapshot.forEach(doc => {
+                    newMessages.push({
+                        id: doc.id,
+                        ...doc.data(),
+                        createdAt: new Date(doc.data().createdAt.toMillis()),
+                    });
+                });
+                cachedMessages(newMessages);
+                setMessages(newMessages);
+            });
+        } else {
+            loadCachedMessages();
+        }
+
+        // clean up code
+        return () => {
+            if (unsubMessages) unsubMessages();
+        }
+    }, [isConnected]);
+
+    // function to create and update message in AsyncStorage
+    const cachedMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem('messages', JSON.stringify(newMessages))
+        } catch (error) {
+            console.log(error.messages);
+        }
+    };
+
+    // function to load messages from AsyncStorage
+    // if messages is not found in AsyncStorage, empty array is returned
+    const loadCachedMessages = async () => {
+        const cachedMessages = await AsyncStorage.getItem('messages') || [];
+        setMessages(JSON.parse(cachedMessages));
+    };
 
     // initialize GiftChat onSend
     const onSend = (newMessages) => {
@@ -40,55 +102,14 @@ const Chat = ({ db, route, navigation }) => {
         )
     };
 
-    // load static system message and user message
-    useEffect(() => {
-        // TESTING ONLY: REMOVED FROM USE
-        // setMessages([
-        //     {
-        //         _id: 1,
-        //         text: "Hello developer",
-        //         createdAt: new Date(),
-        //         user: {
-        //             _id: 2,
-        //             name: "React Native",
-        //             avatar: "https://placeimg.com/140/140/any",
-        //         },
-        //     },
-        //     {
-        //         _id: 2,
-        //         text: 'You have entered the chat room',
-        //         createdAt: new Date(),
-        //         system: true,
-        //     },
-        // ]);
-
-        // define query to fetch messages object and sort by createdAt descending
-        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-
-        // detect messages object changes and update messages object
-        const unsubMessages = onSnapshot(q, (documentSnapshot) => {
-            // load messages
-            let newMessages = [];
-            documentSnapshot.forEach(doc => {
-                newMessages.push({
-                    id: doc.id,
-                    ...doc.data(),
-                    createdAt: new Date(doc.data().createdAt.toMillis()),
-                });
-            });
-            setMessages(newMessages);
-        })
-
-        // clean up code
-        return () => {
-            if (unsubMessages) unsubMessages();
+    // function to render renderInputToolBar component for GiftedChat
+    const renderInputToolbar = (props) => {
+        if (isConnected) {
+            return <InputToolbar {...props} />
+        } else {
+            return null;
         }
-    }, []);
-    
-    // set chatroom title to user specified name
-    useEffect(() => {
-        navigation.setOptions({ title: name });
-    }, []);
+    }
 
     return (
         <View style={[styles.container, {backgroundColor: bgColor}]}>
@@ -96,6 +117,7 @@ const Chat = ({ db, route, navigation }) => {
                 1. load messages
                 2. create text messages
              */}
+            
             <GiftedChat
                 messages={messages}
                 renderBubble={renderBubble}
@@ -104,8 +126,10 @@ const Chat = ({ db, route, navigation }) => {
                     _id: userID,
                     name: name,
                 }}
+                renderInputToolbar={renderInputToolbar}
             />
-
+            
+            
             {/* unhide messages in android when user taps on textarea  */}
             { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
         </View>
